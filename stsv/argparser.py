a9c6097bfeb0a19a040a9d42bdd41f1cd6a43329
Michał Grabowski <mkg.grabowski@gmail.com>
2018-12-13 16:46:59 +0100

    Add an arguments parser

---
+++ stsv/argparser.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+
+
+def parse():
+    parser = ArgumentParser(description="SwipeToSelectViewer - package for easier cleanups of your photos library")
+    parser.add_argument(dest='extension',
+                        type=str,
+                        help='Extension of your photos files')
+    parser.add_argument('-s',
+                        dest='selected',
+                        type=str,
+                        help='target directory name for selected photos',
+                        default='selected',
+                        required=False)
+    parser.add_argument('-d',
+                        dest='discarded',
+                        type=str,
+                        help='target directory name for discarded photos',
+                        default='discarded',
+                        required=False)
+    args = vars(parser.parse_args())
+    return args.get('extension'), args.get('selected'), args.get('discarded')
+
+
+if __name__ == '__main__':
+    print("This is stsv's parser")
