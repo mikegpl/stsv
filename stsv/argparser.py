188c35ebe7407610ebd617adffb463fc9ed27236
Michał Grabowski <mkg.grabowski@gmail.com>
2018-12-15 20:01:59 +0100

    Add screen resolution as non-required argument

--- stsv/argparser.py
+++ stsv/argparser.py
@@ -3,13 +3,21 @@ from argparse import ArgumentParser
 DEFAULT_WORKING_DIRECTORY = '.'
 DEFAULT_SELECTED_DIRECTORY = 'selected'
 DEFAULT_DISCARDED_DIRECTORY = 'discarded'
+DEFAULT_RESOLUTION = '1920x1080'
 
 
 def parse():
     parser = ArgumentParser(description="SwipeToSelectViewer - package for easier cleanups of your photos library")
     parser.add_argument(dest='extension',
                         type=str,
-                        help='Extension of your photos files')
+                        help='Extension of your photos files.')
+    parser.add_argument('-res',
+                        dest='resolution',
+                        type=str,
+                        default=DEFAULT_RESOLUTION,
+                        help='Resolution of your screen. Default format is \'WIDTHxHEIGHT\'.',
+                        required=False
+                        )
     parser.add_argument('-dir',
                         dest='directory',
                         type=str,
@@ -19,17 +27,18 @@ def parse():
     parser.add_argument('-s',
                         dest='selected',
                         type=str,
-                        help='target directory name for selected photos',
+                        help='Target directory name for selected photos.',
                         default=DEFAULT_SELECTED_DIRECTORY,
                         required=False)
     parser.add_argument('-d',
                         dest='discarded',
                         type=str,
-                        help='target directory name for discarded photos',
+                        help='Target directory name for discarded photos.',
                         default=DEFAULT_DISCARDED_DIRECTORY,
                         required=False)
     args = vars(parser.parse_args())
-    return args.get('extension'), args.get('directory'), args.get('selected'), args.get('discarded')
+    results = ['extension', 'resolution', 'directory', 'selected', 'discarded']
+    return [args.get(result) for result in results]
 
 
 if __name__ == '__main__':
