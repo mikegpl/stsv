8084af8da2fb3cbcf20be0414b3b60f5cc6468f9
Michał Grabowski <mkg.grabowski@gmail.com>
2018-12-15 20:03:00 +0100

    Enable scaling displayed pictures to screen size.

--- stsv/interface.py
+++ stsv/interface.py
@@ -8,11 +8,31 @@ MATT_PATH = pkg_resources.resource_filename(__name__, MATT)
 
 class Interface:
     WAIT_INFINITELY = 0
+    RESOLUTION_SEPARATOR = 'x'
 
-    @staticmethod
-    def display_picture(caption, image_path):
+    def __init__(self, resolution_string):
+        [self.screen_width, self.screen_height] = [int(n) for n in
+                                                   resolution_string.split(Interface.RESOLUTION_SEPARATOR)]
+
+    def display_picture(self, caption, image_path):
         cv2.destroyAllWindows()
-        cv2.imshow(caption, cv2.imread(image_path))
+        cv2.namedWindow(caption, cv2.WINDOW_NORMAL)
+
+        picture = cv2.imread(image_path)
+        scaled_width, scaled_height = self.scaled_to_screen_picture_size(picture)
+        cv2.resizeWindow(caption, scaled_width, scaled_height)
+        cv2.imshow(caption, picture)
+
+    def scaled_to_screen_picture_size(self, image):
+        shape = image.shape
+        width, height = shape[1], shape[0]
+
+        scale = 1.0
+        if width > self.screen_width:
+            scale = self.screen_width / width
+        elif height > self.screen_height:
+            scale = self.screen_height / height
+        return int(width * scale), int(height * scale)
 
     @staticmethod
     def get_decision(selector, decisions):
@@ -29,11 +49,11 @@ class Interface:
 
 
 class WelcomingInterface(Interface):
-    def __init__(self, welcome_image_path=MATT_PATH):
+    def __init__(self, resolution_string, welcome_image_path=MATT_PATH):
+        super().__init__(resolution_string)
         self.welcome_image_path = welcome_image_path
 
     def welcome_and_get_decision_key(self):
         print("Press key used for selecting images")
         super().display_picture("Hello there", self.welcome_image_path)
         return super().get_keystroke()
-
--- stsv/main.py
+++ stsv/main.py
@@ -1,22 +1,23 @@
 from stsv.argparser import parse
 from stsv.decisions import DecisionCollector, Decision, DecisionExecutionSupervisor
 from stsv.file_utils import FileUtils
-from stsv.interface import Interface, WelcomingInterface
+from stsv.interface import WelcomingInterface
 
 
 def run():
-    extension, pics_directory, selected_dir, discarded_dir = parse()
-    selector = WelcomingInterface().welcome_and_get_decision_key()
+    extension, resolution_string, pics_directory, selected_dir, discarded_dir = parse()
     collector = DecisionCollector()
+    interface = WelcomingInterface(resolution_string)
+    selector = interface.welcome_and_get_decision_key()
 
     pictures_paths = FileUtils.list_files_by_extension(extension, pics_directory)
     for i, picture_path in enumerate(pictures_paths):
-        caption = "Displaying picture {}. {} of {}".format(picture_path, i, len(pictures_paths))
-        Interface.display_picture(caption, picture_path)
-        decision = Interface.get_decision(selector, Decision.decision_map())
+        caption = "Displaying picture {}. {} of {}".format(picture_path, i + 1, len(pictures_paths))
+        interface.display_picture(caption, picture_path)
+        decision = interface.get_decision(selector, Decision.decision_map())
         collector.record_decision(decision, picture_path)
 
-    Interface.clear_screen()
+    interface.clear_screen()
 
     supervisor = DecisionExecutionSupervisor(pics_directory)
     supervisor.execute(collector, {
